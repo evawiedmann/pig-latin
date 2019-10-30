33bfeb84da63fd879d6d096552c1ddcc0c6a2705
Student <example@example.com>
2019-10-30 11:59:43 -0700

    add forEach loops, and for loop, but no functionallity

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,40 @@
+// function addAy(word) {
+//
+// }
+
+
 $(document).ready(function () {
+  $('form#sentenceForm').submit(function(event){
+    var sentence = $('input#sentenceInput').val();
+    var splitWord = sentence.split('');
+    var vowels = ["a","i","o","u","e"];
+    splitWord.forEach(function(word, vowels){
+      var newArray = []
+      vowels.forEach(function(vowel, i){
+        if (word[0] === vowel[i]){
+          newArray.push('ay');
+        }
+        return newArray;
 
-
-
+    });
+    });
+    // for (var i = 0; i < splitWord.length; i++) {
+    //   if (splitWord[0] === "a", "i", "o", "u", "e") {
+    //      var addAy = splitWord.append('ay');
+    //   }
+    //   return addAy;
+    // }
+    console.log(newArray);
+    event.preventDefault();
+  });
 });
+
+
+// function alphaOnly(event) {
+//   var key = event.keyCode;
+//   if ((key >= 65 && key <= 90) || key == 8){
+//     return true;
+//   } else {
+//     return false
+//   }
+// }
